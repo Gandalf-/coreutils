bf7887628eb201e881d823be91f76de663e22298
Gandalf- <austin.voecks@gmail.com>
2018-11-01 20:39:03 -0700

    add basic sleep

---
+++ sleep.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import           Control.Concurrent (threadDelay)
+import           System.Environment (getArgs)
+import           Text.Read          (readMaybe)
+
+
+sleep :: [Maybe Int] -> IO ()
+sleep (Just time : _) = threadDelay $ time * 1000000
+sleep _               = help
+
+
+help :: IO ()
+help = putStrLn "sleep: [seconds]"
+
+
+main :: IO ()
+main = map readMaybe <$> getArgs >>= sleep
