d9fd5568827ec17506cefc7288389bd452a05be7
Gandalf- <austin.voecks@gmail.com>
2018-11-03 07:45:34 -0700

    minor clean up

--- nologin.hs
+++ nologin.hs
@@ -1,8 +1,11 @@
 module Main where
 
+-- nologin
+--
+-- just say no
+
 import           System.Exit
 
-main :: IO ()
 main = do
       putStrLn "This account is currently not available."
       exitFailure
--- sleep.hs
+++ sleep.hs
@@ -5,10 +5,8 @@ import           System.Environment (getArgs)
 import           Text.Read          (readMaybe)
 
 
-sleep :: [Maybe Int] -> IO ()
 sleep (Just time : _) = threadDelay $ time * 1000000
 sleep _               = putStrLn "sleep: [seconds]"
 
 
-main :: IO ()
 main = map readMaybe <$> getArgs >>= sleep
