7837a96738b5b26eac0e31975ae4734079fc349c
Gandalf- <austin.voecks@gmail.com>
2018-11-03 07:45:45 -0700

    refactor cat to use Either

--- cat.hs
+++ cat.hs
@@ -4,35 +4,43 @@ module Main where
 --
 -- read files from the command line or echo stdin
 
-import           Data.Maybe         (fromMaybe)
+import           Data.Either        (isRight)
 import           System.Directory   (doesFileExist)
 import           System.Environment (getArgs)
+import           System.Exit
 
 
-cat :: [String] -> Maybe String -> IO ()
-cat args content =
-      case args of
-          (file : args) -> do
-              exists <- doesFileExist file
-              if exists
-                then do
-                  fileContent <- readFile file
-                  cat args $ Just (fileContent ++ fromMaybe "" content)
-
-                else putStrLn $
-                  "cat: " ++ file ++ ": No such file or directory"
-
-          [] -> output content
-  where
-      output :: Maybe String -> IO ()
-      output Nothing  = do
-        content <- getContents
-        putStrLn content
-
-      output (Just c) = putStrLn c
+type ErrorOrFile = Either FilePath String
 
 
 main :: IO ()
-main = do
-      args <- getArgs
-      cat args Nothing
+main = getArgs >>= mapM collect >>= cat
+
+
+collect :: FilePath -> IO ErrorOrFile
+-- ^ if the file exists grab it's contents, otherwise just keep the name
+collect file = do
+    exists <- doesFileExist file
+    if exists
+      then Right <$> readFile file
+      else return $ Left file
+
+
+cat :: [ErrorOrFile] -> IO ()
+-- ^ if there are any files display them, otherwise read from stdin
+cat [] = getContents >>= putStr
+cat xs = do
+      mapM_ display xs
+
+      if all isRight xs
+        then exitSuccess
+        else exitFailure
+
+
+display :: ErrorOrFile -> IO ()
+-- ^ print the contents, or the error
+display (Right content) =
+    putStr content
+
+display (Left file)  =
+    putStrLn $ "cat: " ++ file ++ ": No such file or directory"
