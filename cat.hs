9ea4d9025222c4eddc484b0ea112730141a526e1
Gandalf- <austin.voecks@gmail.com>
2018-11-17 20:26:46 -0800

    simplify cat again

--- cat.hs
+++ cat.hs
@@ -3,6 +3,7 @@ module Main where
 -- cat
 --
 -- read files from the command line or echo stdin
+-- soldiers on when some files do not exist, but reports failure at the end
 
 import           Control.Exception  (IOException, try)
 import           Control.Monad      (when)
@@ -10,17 +11,18 @@ import           Data.Either        (isLeft)
 import           System.Environment (getArgs)
 import           System.Exit        (exitFailure)
 
-type ErrorOrFile = Either IOException String
-type ErrorsOrFiles = [ErrorOrFile]
 
 main :: IO ()
+-- ^ attempt to use all arguments as files
 main = getArgs >>= mapM (try . readFile) >>= cat
 
-cat :: ErrorsOrFiles -> IO ()
+
+cat :: [Either IOException String] -> IO ()
 cat [] = getContents >>= putStr
-cat xs = do
-        mapM_ display xs
-        when (any isLeft xs) exitFailure
+
+cat files = do
+        mapM_ display files
+        when (any isLeft files) exitFailure
     where
         display (Left exception) = print exception
         display (Right content)  = putStr content
