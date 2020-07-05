c36b91e089a57646483ed778e6848b01afb6ca08
Gandalf- <austin.voecks@gmail.com>
2020-07-05 10:02:30 -0700

    streaming head

--- Coreutils/Head.hs
+++ Coreutils/Head.hs
@@ -11,9 +11,12 @@ module Coreutils.Head where
 -- this has constant memory usage even with negative values on streams
 
 import           Control.Monad
-import qualified Data.ByteString.Lazy       as L
-import qualified Data.ByteString.Lazy.Char8 as C
-import           GHC.Int                    (Int64)
+import qualified Data.ByteString.Lazy            as L
+import qualified Data.ByteString.Lazy.Char8      as BC
+import qualified Data.ByteString.Streaming       as Q
+import qualified Data.ByteString.Streaming.Char8 as C
+import           GHC.Int                         (Int64)
+import qualified Streaming.Prelude               as S
 import           System.Console.GetOpt
 import           System.Exit
 import           System.IO
@@ -100,68 +103,50 @@ verbose final fn f = do
 headBytes :: Int64 -> File -> IO ()
 -- take some number of bytes from the beginning of the file, or ommit some number from
 -- the end
-headBytes n f@(File h _ s)
-        | n > 0     =
-            if amount > bufferSize
-                then do
-                    readHandle bufferSize >>= L.putStr
-                    headBytes (n - bufferSize) f
-                else readHandle amount >>= L.putStr
-
+headBytes n (File h _ s)
+        | n > 0     = Q.stdout . Q.hGet h $ fromIntegral n
         | n == 0    = pure ()
-
         | otherwise = case s of
             (Just size) ->
                 when (size - amount > 0) $
-                    readHandle (size - amount) >>= L.putStr
+                    Q.hPut stdout . Q.hGet h . fromIntegral $ size - amount
 
             -- it's negative and we have don't have the size
-            Nothing  ->
-                L.hGetContents h >>= chunkedStreamRead L.empty
+            Nothing  -> clever Q.empty $ Q.hGetContents h
     where
-        chunkedStreamRead :: L.ByteString -> L.ByteString -> IO ()
         -- read the stream 'amount' at a time; if we get less than expected we know
         -- we're at the end
-        chunkedStreamRead previous ls = do
-            let new = L.take amount ls
-                len = L.length new
-            if len < amount
-                then L.putStr $
-                    L.take (len + L.length previous - amount) $ previous <> new
-                else do
-                    L.putStr previous
-                    chunkedStreamRead new (L.drop amount ls)
+        clever previous ls = do
+            let new = Q.take amount ls
+            len <- Q.length_ new
 
-        readHandle :: Int64 -> IO L.ByteString
-        readHandle = L.hGet h . fromIntegral
+            if len < fromIntegral amount
+                then do
+                    prev_len <- Q.length_ previous
+                    let size = fromIntegral $ len + prev_len - fromIntegral amount
+                    Q.hPut stdout $ Q.take size $ previous <> new
+                else do
+                    Q.hPut stdout previous
+                    clever new (Q.drop amount ls)
 
         amount = abs n
-        bufferSize = 16384
 
 headLines :: Int64 -> File -> IO ()
 -- take some number of lines from the beginning of the file, or ommit some number from
 -- the end
 headLines n (File h _ _)
-        | n > 0     = lazyLines >>= go n
+        | n > 0     = C.putStr $ C.unlines $ S.take amount $ C.lines $ C.hGetContents h
         | n == 0    = pure ()
         | otherwise = lazyLines >>= out . chunkedStreamRead []
     where
-        go :: Int64 -> [C.ByteString] -> IO ()
-        go remaining ls =
-            if remaining > fromIntegral bufferSize
-                then do
-                    out $ take bufferSize ls
-                    go (remaining - fromIntegral bufferSize) (drop bufferSize ls)
-                else out $ take (fromIntegral remaining) ls
-
-        lazyLines :: IO [C.ByteString]
-        lazyLines = C.lines <$> C.hGetContents h
+        lazyLines :: IO [BC.ByteString]
+        lazyLines = BC.lines <$> BC.hGetContents h
 
         out :: [L.ByteString] -> IO ()
         out [] = pure ()
-        out xs = C.putStrLn $ C.intercalate "\n" xs
+        out xs = BC.putStrLn $ BC.intercalate "\n" xs
 
-        chunkedStreamRead :: [C.ByteString] -> [C.ByteString] -> [C.ByteString]
+        chunkedStreamRead :: [BC.ByteString] -> [BC.ByteString] -> [BC.ByteString]
         -- read the stream 'amount' at a time; if we get less than expected we know
         -- we're at the end
         chunkedStreamRead previous ls = do
@@ -172,8 +157,59 @@ headLines n (File h _ _)
                 else previous <> chunkedStreamRead new (drop amount ls)
 
         amount = fromIntegral $ abs n
-        bufferSize :: Int
-        bufferSize = 4096
+
+        -- chunkedStreamRead :: [C.ByteString] -> [C.ByteString] -> [C.ByteString]
+        -- read the stream 'amount' at a time; if we get less than expected we know
+        -- we're at the end
+        {-
+        clever :: Monad m => C.ByteString m r -> C.ByteString m r -> S.Stream (S.Of (C.ByteString m r)) m ()
+        clever previous ls = do
+            let new = Q.take (fromIntegral amount) ls
+                len = 234
+            -- len <- Q.length_ new
+            if len < amount
+                then do
+                    prev_len <- Q.length_ previous
+                    S.yield $ Q.take (fromIntegral $ len + prev_len - fromIntegral amount) $ previous <> new
+                else undefined -- previous <> clever new (drop amount ls)
+        -}
+
+{-
+g :: S.Stream (Of Integer) IO ()
+g = do
+    S.each [1..4]
+    S.each [5..6]
+    S.each [7..9]
+
+h s = do
+    let a = S.take 5 s
+        b = S.drop 5 s
+    b
+    a
+
+type ByteStream = S.Stream (C.ByteString IO) IO ()
+
+-- f :: S.Stream (Of a) IO () -> Int -> S.Stream (Of a) IO () -> S.Stream (Of a) IO ()
+f :: ByteStream -> Int -> ByteStream -> ByteStream
+f prev n s = do
+    let this = S.take n s
+        more = S.drop n s
+
+    let t = S.length_ $ S.copy this
+    tl <- t
+    -- let tl = S.effects $ S.length $ S.copy this
+
+    if tl < n
+        then do
+            let p = S.length_ $ S.copy prev
+            pl <- p
+            S.take (tl + pl - n) $ do
+                p
+                t
+        else do
+            prev
+            f this n more
+-}
 
 -- | Options
 
