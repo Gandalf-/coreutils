a351f050d1e595268bedd0bcef333e502c7ef932
Gandalf- <austin.voecks@gmail.com>
2022-04-21 20:33:40 -0700

    convert env integration tests

--- test/integration/env.sh
+++ test/integration/env.sh
@@ -12,42 +12,50 @@ f="file.txt"
 cleanup() { rm -f "$f"; }
 trap cleanup EXIT
 
-# empty environment
-env - | expect-empty
-env -i | expect-empty
-env --ignore-environment | expect-empty
+test_empty_environment() {
+    env - | expect-empty
+    env -i | expect-empty
+    env --ignore-environment | expect-empty
+}
 
-# variable reporting
-APPLE=1 env | expect APPLE=1
+test_variable_reporting() {
+    APPLE=1 env | expect APPLE=1
+}
 
-# variable setting
-env APPLE=2 | expect APPLE=2
+test_variable_setting() {
+    env APPLE=2 | expect APPLE=2
+}
 
-# unset variable
-APPLE=1 env -u APPLE | expect-not APPLE
-APPLE=1 env --unset APPLE | expect-not APPLE
-APPLE=1 env APPLE= | expect-not APPLE
+test_unset_variable() {
+    APPLE=1 env -u APPLE | expect-not APPLE
+    APPLE=1 env --unset APPLE | expect-not APPLE
+    APPLE=1 env APPLE= | expect-not APPLE
+}
 
-# execute command
-env sh -c 'echo hello' | expect hello
+test_execute_command() {
+    env sh -c 'echo hello' | expect hello
+}
 
-# change working directory
-env sh -c 'pwd' \
-    | expect-not 'test/integration'
+test_change_working_directory() {
+    env sh -c 'pwd' \
+        | expect-not 'test/integration'
 
-env -C test/integration sh -c 'pwd' \
-    | expect 'test/integration'
+    env -C test/integration sh -c 'pwd' \
+        | expect 'test/integration'
 
-env --chdir test/integration sh -c 'pwd' \
-    | expect 'test/integration'
+    env --chdir test/integration sh -c 'pwd' \
+        | expect 'test/integration'
+}
 
-# multiple variables
-env -i APPLE=1 BLUEBERRY=2 > "$f"
-expect-file "$f" "APPLE=1
+test_multiple_variables() {
+    env -i APPLE=1 BLUEBERRY=2 > "$f"
+    expect-file "$f" "APPLE=1
 BLUEBERRY=2"
+}
 
-# variables passed to subprocess
-env APPLE=SAUCE sh -c 'echo $APPLE' | expect SAUCE
+test_variables_passed_to_subprocess() {
+    env APPLE=SAUCE sh -c 'echo $APPLE' | expect SAUCE
+}
 
 # null output not supported
 # newline="$( echo APPLE=1 | sha1sum )"
@@ -57,4 +65,4 @@ env APPLE=SAUCE sh -c 'echo $APPLE' | expect SAUCE
 # [[ "$newline" != "$nulldel" ]] || die "test assumptions are wrong"
 # [[ "$out" == "$nulldel" ]] || die "output wasn't null delimited"
 
-echo "env tests complete"
+run_tests env
