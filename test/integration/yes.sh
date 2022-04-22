c5ae69680919b437cd1cd58bbc2259b710f9bc37
Gandalf- <austin.voecks@gmail.com>
2022-04-21 20:31:17 -0700

    convert yes integration tests

--- test/integration/yes.sh
+++ test/integration/yes.sh
@@ -5,13 +5,16 @@ name=yes
 real=/usr/bin/yes
 source "$root"/test/integration/common.sh
 
-test 'empty' \
-    '| head -n 50'
+test_empty() {
+    compare '| head -n 50'
+}
 
-test 'string' \
-    'hello | head -n 50'
+test_string() {
+    compare 'hello | head -n 50'
+}
 
-test 'long' \
-    '$(cat LICENSE) | head -n 50'
+test_long() {
+    compare '$(cat LICENSE) | head -n 50'
+}
 
-echo "yes tests complete"
+run_tests yes
