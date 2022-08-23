174223b2d77823c629110d5303304cf08f29df50
Gandalf- <austin@anardil.net>
2022-08-22 20:16:37 -0700

    main.hs restructure

--- src/main.hs
+++ src/main.hs
@@ -1,37 +1,38 @@
 module Main where
 
-import           Data.List          (isPrefixOf, sort)
-import           System.Environment (getArgs, getProgName)
-import           System.Exit        (die)
+import qualified Data.HashMap.Strict as H
+import           Data.List
+import           System.Environment
+import           System.Exit
 
-import           Coreutils.Awk      (Awk (..))
-import           Coreutils.Basename (Basename (..))
-import           Coreutils.Cat      (Cat (..))
-import           Coreutils.Cut      (Cut (..))
-import           Coreutils.Dirname  (Dirname (..))
-import           Coreutils.Echo     (Echo (..))
-import           Coreutils.Env      (Env (..))
-import           Coreutils.Head     (Head (..))
-import           Coreutils.Mkdir    (Mkdir (..))
-import           Coreutils.Nl       (Nl (..))
-import           Coreutils.Nologin  (Nologin (..))
-import           Coreutils.Pwd      (Pwd (..))
-import           Coreutils.Random   (Random (..))
-import           Coreutils.Rev      (Rev (..))
-import           Coreutils.Seq      (Seq (..))
-import           Coreutils.Sleep    (Sleep (..))
-import           Coreutils.Split    (Split (..))
-import           Coreutils.Sponge   (Sponge (..))
-import           Coreutils.Sum      (Sum (..))
-import           Coreutils.Tac      (Tac (..))
-import           Coreutils.Tee      (Tee (..))
-import           Coreutils.Test     (Test (..))
-import           Coreutils.Tr       (Tr (..))
-import           Coreutils.Uniq     (Uniq (..))
-import           Coreutils.Wc       (Wc (..))
-import           Coreutils.Which    (Which (..))
-import           Coreutils.Whoami   (Whoami (..))
-import           Coreutils.Yes      (Yes (..))
+import           Coreutils.Awk       (Awk (..))
+import           Coreutils.Basename  (Basename (..))
+import           Coreutils.Cat       (Cat (..))
+import           Coreutils.Cut       (Cut (..))
+import           Coreutils.Dirname   (Dirname (..))
+import           Coreutils.Echo      (Echo (..))
+import           Coreutils.Env       (Env (..))
+import           Coreutils.Head      (Head (..))
+import           Coreutils.Mkdir     (Mkdir (..))
+import           Coreutils.Nl        (Nl (..))
+import           Coreutils.Nologin   (Nologin (..))
+import           Coreutils.Pwd       (Pwd (..))
+import           Coreutils.Random    (Random (..))
+import           Coreutils.Rev       (Rev (..))
+import           Coreutils.Seq       (Seq (..))
+import           Coreutils.Sleep     (Sleep (..))
+import           Coreutils.Split     (Split (..))
+import           Coreutils.Sponge    (Sponge (..))
+import           Coreutils.Sum       (Sum (..))
+import           Coreutils.Tac       (Tac (..))
+import           Coreutils.Tee       (Tee (..))
+import           Coreutils.Test      (Test (..))
+import           Coreutils.Tr        (Tr (..))
+import           Coreutils.Uniq      (Uniq (..))
+import           Coreutils.Wc        (Wc (..))
+import           Coreutils.Which     (Which (..))
+import           Coreutils.Whoami    (Whoami (..))
+import           Coreutils.Yes       (Yes (..))
 
 import           Coreutils.Util
 
@@ -43,72 +44,48 @@ choose :: String -> IO ()
 choose name
     | "utils" `isPrefixOf` name =
         getArgs >>= dispatch
-
     | otherwise = do
         args <- getArgs
         dispatch $ name : args
 
 dispatch :: [String] -> IO ()
-dispatch ("awk":xs)     = run Awk xs
-dispatch ("basename":xs)= run Basename xs
-dispatch ("cat":xs)     = run Cat xs
-dispatch ("cut":xs)     = run Cut xs
-dispatch ("dirname":xs) = run Dirname xs
-dispatch ("echo":xs)    = run Echo xs
-dispatch ("env":xs)     = run Env xs
-dispatch ("head":xs)    = run Head xs
-dispatch ("mkdir":xs)   = run Mkdir xs
-dispatch ("nl":xs)      = run Nl xs
-dispatch ("nologin":xs) = run Nologin xs
-dispatch ("pwd":xs)     = run Pwd xs
-dispatch ("random":xs)  = run Random xs
-dispatch ("rev":xs)     = run Rev xs
-dispatch ("seq":xs)     = run Seq xs
-dispatch ("sleep":xs)   = run Sleep xs
-dispatch ("split":xs)   = run Split xs
-dispatch ("sponge":xs)  = run Sponge xs
-dispatch ("sum":xs)     = run Sum xs
-dispatch ("tac":xs)     = run Tac xs
-dispatch ("tee":xs)     = run Tee xs
-dispatch ("test":xs)    = run Test xs
-dispatch ("tr":xs)      = run Tr xs
-dispatch ("uniq":xs)    = run Uniq xs
-dispatch ("wc":xs)      = run Wc xs
-dispatch ("which":xs)   = run Which xs
-dispatch ("whoami":xs)  = run Whoami xs
-dispatch ("yes":xs)     = run Yes xs
-dispatch _              = die usage
+dispatch (cmd:args) =
+    H.lookupDefault (const $ die usage) cmd table args
+dispatch _ = die usage
 
 usage :: String
 usage = unlines $ ["", "usage:"] <> commands
     where
-        commands = map ("  " <>) $ sort
-            [ "awk"
-            , "basename"
-            , "cat"
-            , "cut"
-            , "dirname"
-            , "echo"
-            , "env"
-            , "head"
-            , "mkdir"
-            , "nl"
-            , "nologin"
-            , "pwd"
-            , "random"
-            , "rev"
-            , "seq"
-            , "sleep"
-            , "split"
-            , "sponge"
-            , "sum"
-            , "tac"
-            , "tee"
-            , "test"
-            , "tr"
-            , "uniq"
-            , "wc"
-            , "which"
-            , "whoami"
-            , "yes"
-            ]
+        commands = map ("  " <>) $ sort $ H.keys table
+
+table :: H.HashMap String ([String] -> IO ())
+table = H.fromList
+    [ ("awk", run Awk)
+    , ("basename", run Basename)
+    , ("cat", run Cat)
+    , ("cut", run Cut)
+    , ("dirname", run Dirname)
+    , ("echo", run Echo)
+    , ("env", run Env)
+    , ("head", run Head)
+    , ("mkdir", run Mkdir)
+    , ("nl", run Nl)
+    , ("nologin", run Nologin)
+    , ("pwd", run Pwd)
+    , ("random", run Random)
+    , ("rev", run Rev)
+    , ("seq", run Seq)
+    , ("sleep", run Sleep)
+    , ("split", run Split)
+    , ("sponge", run Sponge)
+    , ("sum", run Sum)
+    , ("tac", run Tac)
+    , ("tee", run Tee)
+    , ("test", run Test)
+    , ("tr", run Tr)
+    , ("uniq", run Uniq)
+    , ("wc", run Wc)
+    , ("which", run Which)
+    , ("whoami", run Whoami)
+    , ("yes", run Yes)
+    ]
