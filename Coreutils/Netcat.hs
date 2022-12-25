632aa990954b0dcea7954dfa3d62820680148a0b
Gandalf- <austin@anardil.net>
2022-12-24 16:55:38 -0800

    netcat start

---
+++ Coreutils/Netcat.hs
@@ -0,0 +1,182 @@
+module Coreutils.Netcat where
+
+import           Control.Monad
+import           Coreutils.Util
+import           Data.Char
+import           Data.List
+import           Network.Socket
+import           System.Console.GetOpt
+import           System.Exit
+
+data Netcat = Netcat
+
+instance Util Netcat where
+    run _ = ncMain
+
+-- | IO
+
+ncMain :: [String] -> IO ()
+ncMain args = do
+        unless (null errors) $
+            die $ unlines errors
+        either die (`runNc` other) $
+            foldM (flip id) defaultOptions opts
+    where
+        (opts, other, errors) = getOpt RequireOrder optionDesc args
+
+runNc :: Options -> [String] -> IO ()
+runNc = undefined
+
+execute :: Runtime -> IO ()
+execute rt@RunListen{} = do
+    sList <- socket' rt
+
+    sAddr:_ <- getAddrInfo Nothing (Just "0.0.0.0") (Just $ show $ port rt)
+    setSocketOption sList ReuseAddr 1
+    bind sList (addrAddress sAddr)
+    listen sList 1
+
+    pure ()
+
+execute _ = undefined
+
+-- | Implementation
+
+data Runtime =
+      RunListen {
+          port    :: PortNumber
+        , persist :: Bool
+        , socket' :: IO Socket
+    }
+    | RunConnect {
+          host    :: ServiceName
+        , ports   ::  [PortNumber]
+        , socket' :: IO Socket
+    }
+
+getRuntime :: Options -> [String] -> Either String Runtime
+getRuntime os xs = do
+        runtime <- rt
+        pure $ runtime (getSocket os)
+    where
+        rt
+            | optAction os == Listener = listenerRuntime os xs
+            | otherwise = connectorRuntime os xs
+
+listenerRuntime :: Options -> [String] -> Either String (IO Socket -> Runtime)
+listenerRuntime os [pDesc] = do
+    port <- parsePort pDesc
+    Right $ RunListen port (optPersist os)
+listenerRuntime _ _ = Left "listener usage: [options] port"
+
+connectorRuntime :: Options -> [String] -> Either String (IO Socket -> Runtime)
+connectorRuntime os [name, pDesc] = do
+    ports <- case parsePort pDesc of
+        (Left _)  -> parsePortRange pDesc
+        (Right p) -> Right [p]
+    Right $ RunConnect name ports
+connectorRuntime _ _ = Left "connector usage: [options] hostname ports"
+
+getSocket :: Options -> IO Socket
+getSocket os = socket (optFamily os) (optSocketType os) defaultProtocol
+
+-- | Options
+
+protocolTcp :: ProtocolNumber
+protocolTcp = 6
+
+protocolUdp :: ProtocolNumber
+protocolUdp = 17
+
+parsePort :: String -> Either String PortNumber
+parsePort p = do
+    when (null p) $
+        Left "Port number not provided"
+    unless (all isDigit p) $
+        Left "Port number is not all digits"
+    let num = read p :: Integer
+    unless (num > 0 && num < 65536) $
+        Left "Port number out of range"
+    Right $ read p
+
+parsePortRange :: String -> Either String [PortNumber]
+parsePortRange ps
+        | length is /= 1 = Left "No port range provided"
+        | otherwise = do
+            lp <- parsePort ls
+            hp <- parsePort $ tail hs
+            when (lp > hp) $
+                Left "Port ranges cannot be reversed"
+            Right [lp .. hp]
+    where
+        is = elemIndices '-' ps
+        (ls, hs) = splitAt (head is) ps
+
+
+data Action = Listener | Connector
+    deriving (Eq, Show)
+
+data Options = Options {
+      optSocketType :: SocketType
+    , optFamily     :: Family
+    , optProtocol   :: ProtocolNumber
+    , optStdin      :: Bool
+    , optAction     :: Action
+
+    , optPersist    :: Bool
+    }
+
+defaultOptions :: Options
+defaultOptions = Options {
+      optSocketType = Stream
+    , optFamily     = AF_UNSPEC
+    , optProtocol = protocolTcp
+    , optStdin = True
+    , optAction = Connector
+    , optPersist = False
+    }
+
+optionDesc :: [OptDescr (Options -> Either String Options)]
+optionDesc =
+    [ Option "u" []
+        (NoArg
+            (\opt -> Right opt {
+                optSocketType = Datagram, optProtocol = protocolUdp
+            }))
+        "Use UDP instead of the default TCP"
+
+    , Option "U" []
+        (NoArg
+            (\opt -> Right opt { optFamily = AF_UNIX }))
+        "Use Unix Domain Sockets"
+
+    , Option "4" []
+        (NoArg
+            (\opt -> Right opt { optFamily = AF_INET }))
+        "Force the use of IPv4 only"
+
+    , Option "6" []
+        (NoArg
+            (\opt -> Right opt { optFamily = AF_INET6 }))
+        "Force the use of IPv6 only"
+
+    , Option "d" []
+        (NoArg
+            (\opt -> Right opt { optStdin = False }))
+        "Do not attempt to read from stdin"
+
+    , Option "l" []
+        (NoArg
+            (\opt -> Right opt { optAction = Listener }))
+        "Listen for an incoming connection rather than connect to a remote host"
+
+    , Option "k" []
+        (NoArg
+            (\opt -> Right opt { optPersist = True }))
+        "Keep listening after the first connection is completed"
+
+    , Option "h" ["help"]
+        (NoArg
+            (\_ -> Left $ usageInfo "head" optionDesc))
+        "Show this help text"
+    ]
---
+++ test/NetcatSpec.hs
@@ -0,0 +1,126 @@
+module NetcatSpec where
+
+import           Coreutils.Netcat
+import           Data.Either
+import           Network.Socket
+import           System.IO
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+    basics
+
+    describe "port parsing" $ do
+        it "works" $ do
+            parsePort "3489" `shouldBe` Right 3489
+            parsePort "1" `shouldBe` Right 1
+            parsePort "65535" `shouldBe` Right 65535
+
+        it "errors" $ do
+            parsePort "junk" `shouldSatisfy` isLeft
+            parsePort "0" `shouldSatisfy` isLeft
+            parsePort "65536" `shouldSatisfy` isLeft
+            parsePort "999999" `shouldSatisfy` isLeft
+
+    describe "port range parsing" $ do
+        it "works" $ do
+            parsePortRange "4-10" `shouldBe` Right [4..10]
+            parsePortRange "4-4" `shouldBe` Right [4]
+
+        it "errors" $ do
+            parsePortRange "junk" `shouldSatisfy` isLeft
+            parsePortRange "-5" `shouldSatisfy` isLeft
+            parsePortRange "5-" `shouldSatisfy` isLeft
+            parsePortRange "5-junk" `shouldSatisfy` isLeft
+            parsePortRange "junk-5" `shouldSatisfy` isLeft
+            parsePortRange "10-5" `shouldSatisfy` isLeft
+
+basics :: Spec
+basics = do
+    describe "inet pair" $ do
+        it "v4 tcp works" $ do
+            pair <- v4TcpSocketPair
+            pingPong pair
+
+            pair2 <- tcpSocketPair AF_UNSPEC "127.0.0.1"
+            pingPong pair2
+
+        {-
+        it "v4 udp works" $ do
+            (l, r) <- udpSocketPair AF_INET "127.0.0.1"
+            a <- socketToHandle l ReadWriteMode
+            b <- socketToHandle r ReadWriteMode
+            hPutStrLn a "ping"
+            hGetLine b `shouldReturn` "ping"
+        -}
+
+        it "v6 tcp works" $ do
+            pair1 <- v6TcpSocketPair
+            pingPong pair1
+
+            pair2 <- tcpSocketPair AF_UNSPEC "::1"
+            pingPong pair2
+
+    describe "unix pair" $ do
+        it "tcp works" $ do
+            pair <- socketPair AF_UNIX Stream defaultProtocol
+            pingPong pair
+
+        it "udp works" $ do
+            pair <- socketPair AF_UNIX Datagram defaultProtocol
+            pingPong pair
+
+
+pingPong :: (Socket, Socket) -> IO ()
+pingPong (l, r) = do
+    a <- socketToHandle l ReadWriteMode
+    b <- socketToHandle r ReadWriteMode
+    hPutStrLn a "ping"
+    hPutStrLn b "pong"
+    hGetLine b `shouldReturn` "ping"
+    hGetLine a `shouldReturn` "pong"
+    hClose a
+    hClose b
+
+v4TcpSocketPair :: IO (Socket, Socket)
+v4TcpSocketPair = tcpSocketPair AF_INET "127.0.0.1"
+
+v6TcpSocketPair :: IO (Socket, Socket)
+v6TcpSocketPair = tcpSocketPair AF_INET6 "::1"
+
+udpSocketPair :: Family -> ServiceName -> IO (Socket, Socket)
+udpSocketPair family name = do
+    sList <- socket family Datagram defaultProtocol
+    cSock <- socket family Datagram defaultProtocol
+
+    sAddr:_ <- getAddrInfo Nothing (Just name) (Just testPort)
+    setSocketOption sList ReuseAddr 1
+    bind sList (addrAddress sAddr)
+
+    connect cSock (addrAddress sAddr)
+
+    pure (sList, cSock)
+
+tcpSocketPair :: Family -> ServiceName -> IO (Socket, Socket)
+tcpSocketPair family name = do
+    let hints = defaultHints {
+        addrSocketType = Stream,
+        addrFamily = family,
+        addrProtocol = 6
+    }
+    addr:_ <- getAddrInfo (Just hints) (Just name) (Just testPort)
+    sList <- openSocket addr
+    cSock <- openSocket addr
+
+    setSocketOption sList ReuseAddr 1
+    bind sList (addrAddress addr)
+    listen sList 1
+
+    connect cSock (addrAddress addr)
+    (sSock, _) <- accept sList
+    close sList
+
+    pure (sSock, cSock)
+
+testPort :: String
+testPort = "3488"
