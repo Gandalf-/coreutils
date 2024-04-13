93c1003dc30d06454227cb731882306948c3fd7e
Gandalf- <austin@anardil.net>
2024-04-13 10:59:35 -0700

    fold progress

--- Coreutils/Fold.hs
+++ Coreutils/Fold.hs
@@ -3,29 +3,89 @@ module Coreutils.Fold where
 import           Coreutils.Util
 import           Data.Char
 import           System.Console.GetOpt
+import           System.Exit           (exitFailure)
 
 data Fold = Fold
 
 instance Util Fold where
-    run _ _ = undefined
+    run _ = foldMain
+
+foldMain :: [String] -> IO ()
+foldMain args = case getOpt Permute options args of
+    (o, n, []) -> do
+        let opts = foldl (>>=) (Right defaultOptions) o
+        case opts of
+            Left err -> do
+                putStrLn err
+                exitFailure
+            Right os -> mapM_ (processFile os) n
+    (_, _, errs) -> do
+        mapM_ putStrLn errs
+        exitFailure
+
+processFile :: Options -> FilePath -> IO ()
+processFile opts file = readFile file >>= putStr . foldContent opts
+
+foldContent :: Options -> String -> String
+foldContent opts = unlines . concatMap (foldLine opts) . lines
+
+foldLine :: Options -> String -> [String]
+foldLine opts = go 0 ""
+  where
+    width = optWidth opts
+    go n acc xs
+      | n >= width =
+        let (pre, suf) = splitAtSpace acc
+        in pre : go 0 "" (suf ++ xs)
+      | otherwise =
+        case xs of
+            [] -> [acc]
+            (c:cs) ->
+                let p = positions (optPosition opts) c
+                    n' = n + p
+                in if n' > width && optPretty opts && isSpace c
+                    then acc : go p [c] cs
+                    else go n' (acc ++ [c]) cs
 
 
-folder :: Options -> String -> String
-folder os xs =
-        go 0 (optWidth os) $ enumerate os xs
+splitAtSpace :: String -> (String, String)
+splitAtSpace s = case break isSpace (reverse s) of
+    (a, b) -> (reverse b, reverse a)
 
-enumerate :: Options -> String -> [(Int, Char)]
-enumerate os xs =
-        zip pos xs
-    where
-        pos = map (positions (optPosition os)) xs
 
-go :: Int -> Int -> [(Int, Char)] -> String
-go _ _ [] = ""
-go index len xs@((p, c):ixs)
-    | c == '\n'       = c : go 0 len ixs
-    | p + index > len = '\n' : go (index - len) len xs
-    | otherwise       = c : go (index + p) len ixs
+spaceSplit :: String -> Int -> (String, String)
+spaceSplit xs width = go xs "" 0
+  where
+    go :: String -> String -> Int -> (String, String)
+    go [] acc _ = (acc, "")
+    go (c:cs) acc len
+      | isSpace c =
+          let nextWord = takeWhile (not . isSpace) cs
+              newLen = len + length nextWord + 1
+          in if newLen > width
+             then (acc, dropWhile isSpace cs)
+             else go cs (acc ++ [c] ++ nextWord) newLen
+      | otherwise = go cs (acc ++ [c]) (len + 1)
+
+
+splitter :: (Char -> Int) -> String -> Int -> (String, String)
+splitter widthFn xs maxWidth = go xs "" 0
+  where
+    go :: String -> String -> Int -> (String, String)
+    go [] acc _ = (acc, "")
+    go (c:cs) acc currentWidth
+      | isSpace c =
+          let (nextWord, rest) = break isSpace cs
+              nextWordWidth = sum (map widthFn nextWord)
+              spaceWidth = widthFn c
+              newWidth = currentWidth + spaceWidth + nextWordWidth
+          in if newWidth > maxWidth
+             then (acc, cs)
+             else go rest (acc ++ [c] ++ nextWord) newWidth
+      | otherwise =
+          let charWidth = widthFn c
+          in go cs (acc ++ [c]) (currentWidth + charWidth)
+
 
 data Position = Bytes | Columns
 
@@ -61,14 +121,14 @@ options =
     [ Option "b" []
         (NoArg
             (\opt -> Right opt { optPosition = Bytes }))
-        "Count width in bytes intead of column positions"
+        "Count width in bytes instead of column positions"
 
     , Option "s" []
         (NoArg
             (\opt -> Right opt { optPretty = True }))
         "Break lines on spaces when possible"
 
-    , Option "b" []
+    , Option "w" []
         (ReqArg
             (\arg opt -> (\w -> opt { optWidth = w }) <$> parseWidth arg)
             "width")
--- test/FoldSpec.hs
+++ test/FoldSpec.hs
@@ -1,29 +1,41 @@
 module FoldSpec where
 
 import           Coreutils.Fold
+import           Data.Char
 import           Test.Hspec
+import           Text.Parsec    (space)
 
 spec :: Spec
 spec = do
-    describe "enumerate" $
-        it "works" $ do
-            enumerate os "abc"   `shouldBe` [(1, 'a'), (1, 'b'), (1, 'c')]
-            enumerate os "a\rc"  `shouldBe` [(1, 'a'), (-1, '\r'), (1, 'c')]
-            enumerate os "a\t\b" `shouldBe` [(1, 'a'), (8, '\t'), (-1, '\b')]
+    describe "axioms" $
+        it "isSpace" $ do
+            isSpace ' ' `shouldBe` True
+            isSpace '\n' `shouldBe` True
+            isSpace '\t' `shouldBe` True
+            isSpace '\r' `shouldBe` True
+            isSpace 'a' `shouldBe` False
+
+    describe "spaceSplit" $ do
+        it "bytes works" $ do
+            let bsplit = splitter (positions Bytes)
+            bsplit "abc" 3 `shouldBe` ("abc", "")
+            bsplit "ab\nc" 3 `shouldBe` ("ab", "c")
+            bsplit "abc\nd" 3 `shouldBe` ("abc", "d")
 
     describe "fold" $
         it "works" $ do
-            small "abcde"      `shouldBe` "abc\nde"
-            small "ab\ncd"     `shouldBe` "ab\ncd"
-            small "abc\nd"     `shouldBe` "abc\nd"
-            small "ab\r\r"     `shouldBe` "ab\r\r"
-            small "ab\r\rcde"  `shouldBe` "ab\r\rcde"
-            small "ab\r\rcdef" `shouldBe` "ab\r\rcde\nf"
+            True `shouldBe` True
+            -- small "abcde"      `shouldBe` ["abc", "de"]
+            -- small "ab\ncd"     `shouldBe` "ab\ncd"
+            -- small "abc\nd"     `shouldBe` "abc\nd"
+            -- small "ab\r\r"     `shouldBe` "ab\r\r"
+            -- small "ab\r\rcde"  `shouldBe` "ab\r\rcde"
+            -- small "ab\r\rcdef" `shouldBe` "ab\r\rcde\nf"
 
-            medium "\tabc" `shouldBe` "\ta\nbc"
+            -- medium "\tabc" `shouldBe` "\ta\nbc"
             -- medium "a\tbc" `shouldBe` "a\tb\nc"
             -- medium "\t\tbc" `shouldBe` "\t\n\tb\nc"
     where
         os     = defaultOptions
-        small  = folder defaultOptions { optWidth = 3 }
-        medium = folder defaultOptions { optWidth = 9 }
+        small  = foldLine defaultOptions { optWidth = 3 }
+        medium = foldLine defaultOptions { optWidth = 9 }
